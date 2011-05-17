21ca0a56e8250c489660621a54ca39f69f5e94d1
irjudson <irjudson@gmail.com>
2011-05-17 18:23:54 -0500

    Updating algorithm from recovered code. Now to finish.

--- src/main/java/beamscheduling/Greedy.java
+++ src/main/java/beamscheduling/Greedy.java
@@ -2,6 +2,7 @@ package beamscheduling;
 
 import java.util.*;
 import edu.uci.ics.jung.graph.Graph;
+import edu.uci.ics.jung.graph.UndirectedSparseGraph;
 
 class Greedy {
     Network network;
@@ -11,10 +12,25 @@ class Greedy {
     }
 
     public void solve() {
+        HashSet firstLoop = new HashSet();
         int numVertices = network.getVertexCount();
+        network.calculateBeamSet(1, 20);
 
         // Loop 1: 
-        for(int i = 1; i < numVertices; i++) {
+        for(int i = 0; i < network.subList.length; i++) {
+            Vertex s = network.subList[i];
+            double maxThroughput = 0.0;
+            Vertex bestRelay = null;
+            for(int j = 0; j < network.relayList.length; j++) {
+                Vertex r = network.relayList[j];
+                double throughput = s.calculateThroughput(20, r);
+                if (throughput >= maxThroughput) {
+                    maxThroughput = throughput;
+                    bestRelay = r;
+                }
+            }
+            // Do something with the relay
+            firstLoop.put(s, bestRelay);
         }
 
         // Loop 2: Calculate the greedy reward
