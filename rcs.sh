8fdf6a1ae8f5cef04beb981d211d36ae96f146b7
Ivan R. Judson <irjudson@gmail.com>
2011-07-26 06:47:49 -0600

    simulation runner.

--- rcs.sh
+++ rcs.sh
@@ -6,37 +6,60 @@ JAR="target/beam-scheduling-1.0-jar-with-dependencies.jar"
 
 # Scenario 1: vary channels => 3, 6, 9, 12, 15
 for i in 1 2 3 4 5; do
-    echo "Running test for ${i} channels"
     OUTFILE="rcs-channel-${i}.csv"
-    java -Xms3072m -Xmx4096m -jar ${JAR} -c $i &> ${OUTFILE}
+    if [ ! -f $OUTFILE ]; then 
+	echo "Running test for ${i} channels"
+	java -Xms3072m -Xmx4096m -jar ${JAR} -c $i &> ${OUTFILE}
+    fi
 done
 
 # Scenario 2: vary size, keep density constant
 # Width/Height  30 40 50 60 70 80
 # Nodes          9 16 25 36 49 64
-echo "Running test for area of 30km x 30km with 9 nodes"
-java -Xms3072m -Xmx4096m -jar ${JAR} -w 30000.0 -h 30000.0 -n 9 &> rcs-area-30.9.csv
+OUTFILE="rcs-area-30.9.csv"
+if [ ! -f $OUTFILE ]; then 
+    echo "Running test for area of 30km x 30km with 9 nodes"
+    java -Xms3072m -Xmx4096m -jar ${JAR} -w 30000.0 -h 30000.0 -n 9 &> $OUTFILE
+fi
 
-echo "Running test for area of 40km x 40km with 16 nodes"
-java -Xms3072m -Xmx4096m -jar ${JAR} -w 40000.0 -h 40000.0 -n 16 &> rcs-area-40.16.csv
+OUTFILE="rcs-area-40.16.csv"
+if [ ! -f $OUTFILE ]; then 
+    echo "Running test for area of 40km x 40km with 16 nodes"
+    java -Xms3072m -Xmx4096m -jar ${JAR} -w 40000.0 -h 40000.0 -n 16 &> $OUTFILE
+fi
 
-echo "Running test for area of 50km x 50km with 25 nodes"
-java -Xms3072m -Xmx4096m -jar ${JAR} -w 50000.0 -h 50000.0 -n 25 &> rcs-area-50.25.csv
+OUTFILE="rcs-area-50.25.csv"
+if [ ! -f $OUTFILE ]; then 
+    echo "Running test for area of 50km x 50km with 25 nodes"
+    java -Xms3072m -Xmx4096m -jar ${JAR} -w 50000.0 -h 50000.0 -n 25 &> $OUTFILE
+fi
 
-echo "Running test for area of 60km x 60km with 36 nodes"
-java -Xms3072m -Xmx4096m -jar ${JAR} -w 60000.0 -h 60000.0 -n 36 &> rcs-area-60.36.csv
+OUTFILE="rcs-area-60.36.csv"
+if [ ! -f $OUTFILE ]; then 
+    echo "Running test for area of 60km x 60km with 36 nodes"
+    java -Xms3072m -Xmx4096m -jar ${JAR} -w 60000.0 -h 60000.0 -n 36 &> $OUTFILE
+fi
 
-echo "Running test for area of 70km x 70km with 49 nodes"
-java -Xms3072m -Xmx4096m -jar ${JAR} -w 70000.0 -h 70000.0 -n 49 &> rcs-area-70.49.csv
+OUTFILE="rcs-area-70.49.csv"
+if [ ! -f $OUTFILE ]; then 
+    echo "Running test for area of 70km x 70km with 49 nodes"
+    java -Xms3072m -Xmx4096m -jar ${JAR} -w 70000.0 -h 70000.0 -n 49 &> $OUTFILE
+fi
 
-echo "Running test for area of 80km x 80km with 64 nodes"
-java -Xms3072m -Xmx4096m -jar ${JAR} -w 80000.0 -h 80000.0 -n 64 &> rcs-area-80.64.csv
+
+OUTFILE="rcs-area-80.64.csv"
+if [ ! -f $OUTFILE ]; then 
+    echo "Running test for area of 80km x 80km with 64 nodes"
+    java -Xms3072m -Xmx4096m -jar ${JAR} -w 80000.0 -h 80000.0 -n 64 &> rcs-area-80.64.csv
+fi
 
 # Scenario 3: vary node density
 # Width/Height: 50
 # Nodes 9 16 25 36 49 64
 for i in 9 16 25 36 49 64; do
-    echo "Running test for ${i} channels"
     OUTFILE="rcs-density-${i}.csv"
+    if [ ! -f $OUTFILE ]; then 
+    echo "Running test for ${i} channels"
     java -Xms3072m -Xmx4096m -jar ${JAR} -n $i &> ${OUTFILE}
+    fi
 done
